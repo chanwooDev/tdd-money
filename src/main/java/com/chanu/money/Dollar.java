c314f4d43f0e0bc2229ae591355da7773f5f91b6
CHANWOO BAE <39613774+chanwooDev@users.noreply.github.com>
2023-06-26 14:45:03 +0900

    $5 * 2 = $10
    $5 + 10CHF = $10 (환율이 2:1 일경우)
    $5 * 2 = $10 (해결)
    amount를 private으로
    Dollar의 부작용?
    Money 반올림?

---
+++ src/main/java/com/chanu/money/Dollar.java
@@ -0,0 +1,12 @@
+package com.chanu.money;
+
+public class Dollar {
+    int amount;
+    public Dollar(int amount) {
+        this.amount = amount;
+    }
+
+    public void times(int multiplier) {
+        amount = amount * multiplier;
+    }
+}
